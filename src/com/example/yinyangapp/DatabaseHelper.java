2636f0f641bbc451f9efe37629f616bcc4f9986f
Mikey <mikey.rybacki@gmail.com>
2013-04-16 16:24:51 +0200

    Removed unnecessary test lines

--- src/com/example/yinyangapp/DatabaseHelper.java
+++ src/com/example/yinyangapp/DatabaseHelper.java
@@ -1,14 +1,5 @@
 package com.example.yinyangapp;
 
 public class DatabaseHelper {
-	// test 15.29
-	//comment1
-	//comment1
-	//WAIT FOR IT
-	
-	//Testing pull...
-	//testing rebased branch...
-	//Testing pull from Irins
-	
-	//Testing push from MR
+
 }
