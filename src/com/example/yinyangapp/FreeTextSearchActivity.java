12deba8fdd8ccfc3560722765e628bc7a9e3d26a
Ana <constantin.ana89@gmail.com>
2013-04-18 22:11:30 +0200

    s01-s02: commit before merging with the DB branch

--- src/com/example/yinyangapp/FreeTextSearchActivity.java
+++ src/com/example/yinyangapp/FreeTextSearchActivity.java
@@ -3,8 +3,12 @@ package com.example.yinyangapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class FreeTextSearchActivity extends Activity {
+
+	private String textSearch;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,11 @@ public class FreeTextSearchActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.free_text_search, menu);
 		return true;
+	}
+	
+	public void searchFreeText(View view){
+		EditText editText = (EditText) findViewById(R.id.stringSearch);
+		textSearch = editText.getText().toString();
 	}
 
 }
