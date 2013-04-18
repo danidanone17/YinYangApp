ff17d8c64f83dad0c8be1220ded8584e58b52366
fredrikaxelsson <fr.axelsson@gmail.com>
2013-04-18 15:01:09 +0200

    Squashed commit of the following:
    commit be844c0c67066655bb513caae335c6e11c57e8f0
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 14:13:24 2013 +0200
    
    	Vote + Post
    
    commit cb37269dfacffd5b6da88d2e9011ebad2a7b654b
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 12:16:00 2013 +0200
    
    	added the database file
    
    commit 62e42823a1e76173a554803dc9171c0fb1eefd3d
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 11:54:13 2013 +0200
    
    	Working database for all DatabaseType classes
    
    commit 319595fadb9a557c63b5f3de118c129a773ad79d
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 11:30:10 2013 +0200
    
    	Working Comments + common DatabaseType ancestor
    
    commit 23cdecbb4540c49e06b5226f2d8a28edd6b9bf00
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 10:31:46 2013 +0200
    
    	Working db for users
    
    commit b695e01945301c83e3070dee2916a4fe0c8754af
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 09:35:46 2013 +0200
    
    	1st working db
    
    commit 8a87018dd1ab2455090c5cdb97b108619fdc84e6
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Thu Apr 18 08:55:52 2013 +0200
    
    	Moved db classes to separate package
    
    commit ec248139dd39de3fe3d01fe16b3cf02ea9c98217
    Author: Irin <Irin@129.16.198.60>
    Date:   Tue Apr 16 17:05:20 2013 +0200
    
    	yay
    
    commit 2636f0f641bbc451f9efe37629f616bcc4f9986f
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 16:24:51 2013 +0200
    
    	Removed unnecessary test lines
    
    commit 7ccff81ea214d27e134cc2fa530cb58621c1de20
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 16:23:18 2013 +0200
    
    	Bad
    
    commit 3bfa33961583a8c4644d20335258a2220d4570ea
    Author: Irin <Irin@129.16.198.60>
    Date:   Tue Apr 16 16:18:07 2013 +0200
    
    	blabla
    
    commit cecca1df77884a6931596f83933ae6ae3e8720b5
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 16:14:05 2013 +0200
    
    	)_
    
    commit a073217178d56ae088ff5fcaca535a7b27aebaa6
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:59:49 2013 +0200
    
    	__
    
    commit 94366bdfdae42d3c6bc5a50b40b109547ad20fc9
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:54:11 2013 +0200
    
    	Wait for it
    
    commit 494373ef2940a267305fec82b992fcbf7b9ce688
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:37:02 2013 +0200
    
    	??
    
    commit 355f0fe0e56f49d38db61f0be5c3502d6a80745d
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:29:11 2013 +0200
    
    	Test
    
    commit d3acc486ababaaa9cff1e7c533d2d188da83fb85
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:25:10 2013 +0200
    
    	?
    
    commit b4b28663e0c458ecc4cd2e72d5eddbdc569f0c66
    Author: Mikey <mikey.rybacki@gmail.com>
    Date:   Tue Apr 16 15:05:01 2013 +0200
    
    	Test commit

--- src/com/example/yinyangapp/MainActivity.java
+++ src/com/example/yinyangapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.yinyangapp;
 
+import java.util.ArrayList;
 
 import com.example.yinyangapp.R;
+import com.example.yinyangapp.database.DatabaseAdapter;
+import com.example.yinyangapp.databaseentities.DatabaseType;
+import com.example.yinyangapp.databaseentities.User;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.database.Cursor;
@@ -10,22 +15,35 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-	
-	
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+	public void testDB() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		DatabaseAdapter mDbHelper = new DatabaseAdapter(getBaseContext());
+		mDbHelper.createDatabase();
+		mDbHelper.open();
+
+		ArrayList<DatabaseType> users = mDbHelper.getVotes();
+		TextView text01 = (TextView) findViewById(R.id.text01);
+		String text = "";
+		for (DatabaseType u : users) {
+			text += u.toString() + " ";
+		}
+		text01.setText(text);
+		mDbHelper.close();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		testDB();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
