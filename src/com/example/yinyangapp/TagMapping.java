11d5f79d0964a8d9145e4408bc9ed2d8e2ac0406
Ana <constantin.ana89@gmail.com>
2013-04-22 16:55:04 +0200

    s02-us38: trying the push

--- src/com/example/yinyangapp/TagMapping.java
+++ src/com/example/yinyangapp/TagMapping.java
@@ -1,7 +1,12 @@
 package com.example.yinyangapp;
 
+import java.util.ArrayList;
+
 public class TagMapping {
 	
-	
+	String tag1;
+	String tag2;
+	ArrayList<String> postTagLines;
+	ArrayList<String> mappingTagLines;
 
 }
