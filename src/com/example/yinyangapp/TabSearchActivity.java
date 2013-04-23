b80a01dc7724d59183d2d076932e4ad16e61ab62
danidanone17 <danyholzner@freenet.de>
2013-04-23 13:58:44 +0200

    us40 - Prepared buttons for future functionality

--- src/com/example/yinyangapp/TabSearchActivity.java
+++ src/com/example/yinyangapp/TabSearchActivity.java
@@ -1,8 +1,12 @@
 package com.example.yinyangapp;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.LightingColorFilter;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class TabSearchActivity extends Activity {
 
@@ -10,6 +14,9 @@ public class TabSearchActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tab_search);
+		
+		Button button = (Button)this.findViewById(R.id.button_center);
+		button.getBackground().setColorFilter(0xA0149EE3, PorterDuff.Mode.MULTIPLY);
 	}
 
 	@Override
@@ -17,6 +24,27 @@ public class TabSearchActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tab_search, menu);
 		return true;
+	}
+	
+	public void moveForwards(View view){
+		Button button = (Button)view;
+		button.setText("Hej");
+	}
+	
+	public void moveBackwards(View view){
+		Button button = (Button)view;
+		button.setText("Ho");
+	}
+	
+	public void moveToCenter(View view){
+		Button button = (Button)view;
+		
+		String text = button.getId() + "";
+		
+		Button centerButton = (Button)this.findViewById(R.id.button_center);
+		centerButton.setText(text);
+		
+
 	}
 
 }
