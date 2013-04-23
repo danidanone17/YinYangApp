7b09cd423e79fe5b6b72811ffc1c56b2f0b91c34
fredrikaxelsson <fr.axelsson@gmail.com>
2013-04-23 14:19:50 +0200

    dev: Merged the user profile (us35) into the development

--- src/com/example/yinyangapp/MainActivity.java
+++ src/com/example/yinyangapp/MainActivity.java
@@ -8,36 +8,46 @@ import com.example.yinyangapp.databaseentities.*;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-	public void test(){
+
+	// Called when you click the testSearch-Button
+	// Displays a test search performed in the Controller
+	public void testSearch(View view) {
 		Controller controller = new Controller();
-		ArrayList<DatabaseType> users = controller.testSearch(getBaseContext());
+		ArrayList<DatabaseType> users = controller.testSearch(this);
 		TextView text01 = (TextView) findViewById(R.id.text01);
-		String text="";
-		for(DatabaseType u : users) {
+		String text = "";
+		for (DatabaseType u : users) {
 			User user = (User) u;
-			text+=user.getDisplayName()+"\n";
+			text += user.getDisplayName() + "\n";
 		}
 		text01.setText(text);
 	}
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        test();
-    }
 
+	// Called when you click the testUserProfileButton
+	// Go to the user profile view for the specified user id 106
+	public void testUserProfile(View view) {
+		Intent intent = new Intent(this, UserProfileActivity.class);
+		intent.putExtra(UserProfileActivity.EXTRA_USERID, 106);
+		startActivity(intent);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
---
+++ src/com/example/yinyangapp/UserProfileActivity.java
@@ -0,0 +1,167 @@
+package com.example.yinyangapp;
+
+import java.text.SimpleDateFormat;
+
+import com.example.yinyangapp.controller.Controller;
+import com.example.yinyangapp.database.DatabaseAdapter;
+import com.example.yinyangapp.databaseentities.User;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.text.Html;
+import android.view.*;
+import android.widget.TextView;
+
+public class UserProfileActivity extends Activity {
+
+	public final static String EXTRA_USERID = "com.example.YingYangApp.USERID";
+	public final static String EXTRA_DB_HELPER = "com.example.YingYangApp.DBHELPER";
+	private int userId = 304;
+	private DatabaseAdapter mDbHelper;
+	private int reputation;
+	private String name;
+	private String creationDate;
+	private String lastAccessDate;
+	private String website;
+	private String location;
+	private int age;
+	private String description;
+	private int profileViews;
+	private int downVotes;
+	private int upVotes;
+	private Controller controller;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_profile);
+		Intent intent = getIntent();
+		userId = intent.getIntExtra(EXTRA_USERID, -1);
+		mDbHelper = new DatabaseAdapter(getBaseContext());
+		controller = new Controller();
+		try {
+			getDbInformation();
+		} catch (java.text.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		updateView();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.user_profile, menu);
+		return true;
+	}
+
+	private void getDbInformation() throws java.text.ParseException {
+		mDbHelper.createDatabase();
+		mDbHelper.open();
+
+		User user = mDbHelper.getUser(userId);
+
+		reputation = user.getReputation();
+		name = user.getDisplayName();
+		website = user.getWebsiteUrl();
+		location = user.getLocation();
+		age = user.getAge();
+		description = user.getAboutMe();
+		profileViews = user.getViews();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		creationDate = user.getCreationDate();
+		lastAccessDate = user.getLastAccessDate();
+
+		description.replaceAll("\"", "\\\"");
+
+		System.out.println(description);
+
+		mDbHelper.close();
+	}
+
+	private void updateView() {
+		setReputationText();
+		setUserNameText();
+		setCreationDateText();
+		setLastAccessDate();
+		setLocation();
+		setWebsite();
+		setDescription();
+		setProfileViews();
+		setAge();
+
+	}
+
+	private void setReputationText() {
+		TextView textView = (TextView) findViewById(R.id.reputationScore);
+		textView.setText("" + reputation);
+	}
+
+	private void setUserNameText() {
+		TextView textView = (TextView) findViewById(R.id.userName);
+		textView.setText(name);
+	}
+
+	private void setCreationDateText() {
+		TextView textView = (TextView) findViewById(R.id.membershipDuration);
+		textView.setText(controller.getDateDifference(creationDate));
+
+	}
+
+	private void setLastAccessDate() {
+		TextView textView = (TextView) findViewById(R.id.lastSeen);
+		textView.setText(controller.getDateDifference(lastAccessDate));
+	}
+
+	private void setWebsite() {
+		TextView textView = (TextView) findViewById(R.id.website);
+		textView.setText(website);
+	}
+
+	private void setLocation() {
+		TextView textView = (TextView) findViewById(R.id.location);
+		textView.setText(location);
+	}
+
+	private void setAge() {
+		TextView textView = (TextView) findViewById(R.id.age);
+		textView.setText("" + age);
+	}
+
+	private void setDescription() {
+		TextView textView = (TextView) findViewById(R.id.userDescription);
+		textView.setText(Html.fromHtml(description));
+		// textView.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+
+	private void setProfileViews() {
+		TextView textView = (TextView) findViewById(R.id.profileViews);
+		textView.setText("" + profileViews);
+	}
+
+	// Do things when the buttons is pressed
+	public void favoriteQuestionsView(View view) {
+	}
+
+	public void reputationView(View view) {
+	}
+
+	public void answersView(View view) {
+	}
+
+	public void questionsView(View view) {
+	}
+
+	public void tagsView(View view) {
+	}
+
+	public void badgesView(View view) {
+	}
+
+	public void activitiesView(View view) {
+	}
+
+}
