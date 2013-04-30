9422ac45df9b91f83551b3c02350179ad3216bd0
Mikey <mikey.rybacki@gmail.com>
2013-04-30 09:01:20 +0200

    s03u46 - Created a test project within the project

---
+++ tests/src/com/yinyang/so/test/QuestionControllerValidation.java
@@ -0,0 +1,11 @@
+package com.yinyang.so.test;
+
+import junit.framework.TestCase;
+
+public class QuestionControllerValidation extends TestCase {
+
+	public QuestionControllerValidation(String name) {
+		super(name);
+	}
+
+}
