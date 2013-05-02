59cb8f792c33307d2037b7edf7e4b8df4bc6b0a5
Mikey <mikey.rybacki@gmail.com>
2013-05-02 09:59:43 +0200

    tests: some tests in QuestionControllerValidation

--- tests/src/com/yinyang/so/test/DatabaseAdapterValidation.java
+++ tests/src/com/yinyang/so/test/DatabaseAdapterValidation.java
@@ -1,9 +1,12 @@
 package com.yinyang.so.test;
 
-import android.util.Log;
+import java.util.ArrayList;
 
 import com.yinyang.so.database.DatabaseAdapter;
-import com.yinyang.so.database.SoData;
+import com.yinyang.so.database.MeanOfSearch;
+import com.yinyang.so.database.SearchEntity;
+import com.yinyang.so.databaseentities.Post;
+
 
 public class DatabaseAdapterValidation extends 
 	android.test.InstrumentationTestCase {
@@ -23,10 +26,59 @@ public class DatabaseAdapterValidation extends
 		db.close();
 	}
 	
-	public void testFirstIndex() {
-
+	// tests the getUsers() method, which should
+	// return 20 entries
+	public void testGetUsers() {
 		int index = db.getUsers().size();
 		assertEquals(index, 20);
-		
 	}
+	
+	// tests the getVotes() method, which should
+	// return 20 entries
+	public void testGetVotes() {
+		int index = db.getVotes().size();
+		assertEquals(index, 20);
+	}
+	
+	
+	// get posts from getDataByCriteria
+	public void testGetDataByCriteria() {
+		// TODO: insert data into database
+		// TODO: test if data inserted
+	}
+	
+	// tests the getComments() method, which should
+	// return 20 entries
+	public void testGetComments() {
+		int index = db.getComments().size();
+		assertEquals(index, 20);
+	}
+	
+	// tests the getPosts() method, which should
+	// return 20 entries
+	public void testGetPosts() {
+		int index = db.getPosts().size();
+		assertEquals(index, 20);
+	}
+	
+	// tests the getCountByCriteria() method, searches for java in post body
+	// fails if under 1 entry found
+	public void testGetCountByCriteria() {
+		SearchEntity se = new SearchEntity(Post.KEY_BODY, "java",
+				MeanOfSearch.contained);
+		ArrayList<SearchEntity> searchEntities = new ArrayList<SearchEntity>();
+		searchEntities.add(se);
+		int index = db.getCountByCriteria(Post.TABLE_NAME,
+				searchEntities);
+		assertTrue(index > 0);
+	}
+	
+	// tests the getLastIndex() and getFirstIndex() methods
+	// comparing the result between them
+	public void testGetFirstLastIndex() {
+		int lastIndex = db.getLastIndex(Post.TABLE_NAME);
+		int firstIndex = db.getFirstIndex(Post.TABLE_NAME);
+		assertTrue(firstIndex < lastIndex);
+	}
+	
 }
--- tests/src/com/yinyang/so/test/QuestionControllerValidation.java
+++ tests/src/com/yinyang/so/test/QuestionControllerValidation.java
@@ -1,24 +1,65 @@
 package com.yinyang.so.test;
 
+import java.util.ArrayList;
+
 import com.yinyang.so.controllers.QuestionController;
+import com.yinyang.so.databaseentities.DatabaseType;
+import com.yinyang.so.databaseentities.Post;
 
 public class QuestionControllerValidation extends 
-	android.test.AndroidTestCase {
+	android.test.InstrumentationTestCase {
 
 	private QuestionController qc;
 	
 	protected void setUp() throws Exception {
 	    super.setUp();
-	    qc = new QuestionController(getContext());
+	    qc = new QuestionController(this.getInstrumentation().getTargetContext().
+	    		getApplicationContext());
 	}
 	
 	protected void tearDown() throws Exception {
 	    super.tearDown();
 	}
 	
-	public void testRandom() {
-		assertEquals(qc.getPost(8414075).getScore(), 1);
-		
+	// Tests getPosts()
+	// Note: HARDCODED VALUES... If this test fails,
+	// it may depend on changes in the db...
+	// TODO: Make it independent
+	public void testGetPost() {
+		assertEquals(qc.getPost(8414075).getAcceptedAnswerId(), 8414247);
+		assertEquals(qc.getPost(8414075).getViewCount(), 65);
+		assertEquals(qc.getPost(8414075).getAnswerCount(), 2);
+	}
+	
+	// Tests getUser()
+	// Note: HARDCODED VALUES... If this test fails,
+	// it may depend on changes in the db...
+	// TODO: Make it independent
+	public void testGetUser() {
+		assertEquals(qc.getUser(13).getAge(), 32);
+		assertEquals(qc.getUser(13).getReputation(), 63611);
+		assertEquals(qc.getUser(13).getViews(), 3347);
+	}
+	
+	// Tests getAnswers()
+	// Note: HARDCODED VALUES... If this test fails,
+	// it may depend on changes in the db...
+	// TODO: Make it independent
+	public void testGetAnswers() {
+		ArrayList<DatabaseType> answers = qc.getAnswers(8414075);
+		assertEquals(answers.size(), 2);
+		Post ans1 = (Post) answers.get(0);
+		Post ans2 = (Post) answers.get(1);
+		assertEquals(ans1.getId(), 8414109);
+		assertEquals(ans2.getId(), 8414247);
+	}
+	
+	// Tests getAcceptedAnswer()
+	// Note: HARDCODED VALUES... If this test fails,
+	// it may depend on changes in the db...
+	// TODO: Make it independent
+	public void testGetAcceptedAnswer() {
+		assertEquals(qc.getAcceptedAnswer(8414075).getId(), 8414247);
 	}
 
 }
