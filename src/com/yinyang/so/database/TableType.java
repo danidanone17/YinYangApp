9156331782fd1a6a27813bc09e1b0279478edb0a
Mikey <mikey.rybacki@gmail.com>
2013-05-03 23:25:53 +0200

    s03u45 - fixed repo problems

---
+++ src/com/yinyang/so/database/TableType.java
@@ -0,0 +1,25 @@
+package com.yinyang.so.database;
+
+import com.yinyang.so.databaseentities.Comment;
+import com.yinyang.so.databaseentities.Post;
+import com.yinyang.so.databaseentities.Tag;
+import com.yinyang.so.databaseentities.User;
+import com.yinyang.so.databaseentities.Vote;
+
+public enum TableType {
+	posts(Post.TABLE_NAME),
+	users(User.TABLE_NAME),
+	comments(Comment.TABLE_NAME),
+	votes(Vote.TABLE_NAME),
+	tags(Tag.TABLE_NAME);
+	
+	private final String value;
+	
+	private TableType(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+}
