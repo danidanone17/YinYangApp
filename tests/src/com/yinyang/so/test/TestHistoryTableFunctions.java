7f365e201a813c616ad909803f42d7525b649027
unknown <constantin.ana89@gmail.com>
2013-05-10 13:38:49 +0200

    test

--- tests/src/com/yinyang/so/test/TestHistoryTableFunctions.java
+++ tests/src/com/yinyang/so/test/TestHistoryTableFunctions.java
@@ -3,5 +3,10 @@ package com.yinyang.so.test;
 import junit.framework.TestCase;
 
 public class TestHistoryTableFunctions extends TestCase {
+	
+	//test for no tag contained in the question is existent in the history table
+	public void testViewQuestion1(){
+		
+	}
 
 }
