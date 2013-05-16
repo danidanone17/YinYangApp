e5a15963eb756606e2a262f8d6be27a792fb9ada
fredrikaxelsson <fr.axelsson@gmail.com>
2013-05-16 11:17:27 +0200

    s05us65-68-gui: working buttons for sort by user rep, question score, creation date, answer count

--- src/com/yinyang/so/activities/SearchResultActivity.java
+++ src/com/yinyang/so/activities/SearchResultActivity.java
@@ -1,18 +1,14 @@
 package com.yinyang.so.activities;
 
-import com.yinyang.so.extras.PredicateLayout;
 import java.util.ArrayList;
 
-import com.yinyang.so.R;
-import com.yinyang.so.databaseentities.Post;
-
-import android.os.Bundle;
-import android.os.Parcelable;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,36 +16,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.yinyang.so.R;
+import com.yinyang.so.databaseentities.Post;
+import com.yinyang.so.extras.PredicateLayout;
+
 public class SearchResultActivity extends Activity {
+	
 	private PostArrayAdapter postArrayAdapter;
 	private ArrayList<Post> posts;
+	private Intent mIntent;
+	private ListView mQuestionList;
 	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search_result);
-		
+
 		// Show the Up button in the action bar.
 		setupActionBar();
-				
+		
+		mQuestionList = (ListView) findViewById(R.id.activity_search_result);
+
 		// get posts to display
-		Intent oIntent = getIntent();
-		posts = oIntent.getParcelableArrayListExtra("POSTS_QUESTION_SCORE");
-		setTitle(getTitle() + " " + oIntent.getStringExtra("TEXT_SEARCH"));
-		
-		// initialize post array adapter
-		postArrayAdapter = new PostArrayAdapter(this,
-				R.layout.search_result_layout, posts);
-		
-		// set post array adapter for list view
-		ListView listView = (ListView) findViewById(R.id.activity_search_result);
-		listView.setAdapter(postArrayAdapter);
+		mIntent = getIntent();
+		getPostByOrder("POSTS_QUESTION_SCORE");
+		setTitle(getTitle() + " " + mIntent.getStringExtra("TEXT_SEARCH"));
 	}
-	
+
 	/**
 	 * Set up the {@link android.app.ActionBar}.
 	 */
@@ -64,7 +61,7 @@ public class SearchResultActivity extends Activity {
 		getMenuInflater().inflate(R.menu.search_result, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -82,24 +79,47 @@ public class SearchResultActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	private void initPostArrayAdapter() {
+		postArrayAdapter = new PostArrayAdapter(this, R.layout.search_result_layout, posts);
+		mQuestionList.setAdapter(postArrayAdapter);
+	}
+	// User one of POSTS_USER_REPUTATION, POSTS_QUESTION_SCORE, POSTS_CREATION_DATE, POSTS_ANSWER_COUNT
+	private void getPostByOrder(String order) {
+		posts = mIntent.getParcelableArrayListExtra(order);
+		initPostArrayAdapter();
+	}
+
+	public void sortByUserReputation(View view) {
+		getPostByOrder("POSTS_USER_REPUTATION");
+	}
+
+	public void sortByQuestionScore(View view) {
+		getPostByOrder("POSTS_QUESTION_SCORE");
+	}
+
+	public void sortByCreationDate(View view) {
+		getPostByOrder("POSTS_CREATION_DATE");
+	}
+
+	public void sortByAnswerCount(View view) {
+		getPostByOrder("POSTS_ANSWER_COUNT");
+	}
+
 	private class PostArrayAdapter extends ArrayAdapter<Post> {
 
 		private ArrayList<Post> posts;
-		
-		public PostArrayAdapter(Context context, int resource, ArrayList<Post> posts) {
+
+		public PostArrayAdapter(Context context, int resource,
+				ArrayList<Post> posts) {
 			super(context, resource, posts);
 			this.posts = posts;
 		}
-		
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 
-			View v = convertView;
-
-			if (v == null) {	
-				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				v = vi.inflate(R.layout.search_result_layout, null);
-			} 
+			LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = vi.inflate(R.layout.search_result_layout, parent, false);
 			
 			Post o = posts.get(position);
 
@@ -109,51 +129,59 @@ public class SearchResultActivity extends Activity {
 				if (tt != null) {
 					tt.setText(Integer.toString(o.getAnswerCount()));
 				}
-			
+
 				// set title
 				TextView title = (TextView) v.findViewById(R.id.title);
 				if (title != null) {
 					title.setText(o.getTitle());
+					Log.e("TEST", "ID: " + o.getId());
 					title.setId(o.getId());
+					
 					title.setOnClickListener(new View.OnClickListener() {
 						@Override
 						public void onClick(View view) {
-							TextView title = (TextView)view;
-							Intent oIntent = new Intent(SearchResultActivity.this,QuestionActivity.class);
-							oIntent.putExtra(com.yinyang.so.activities.QuestionActivity.EXTRA_QUESTIONID, title.getId());
+							TextView title = (TextView) view;
+							Intent oIntent = new Intent(
+									SearchResultActivity.this,
+									QuestionActivity.class);
+							Log.e("", "id onClick: " + title.getId());
+							oIntent.putExtra(
+									com.yinyang.so.activities.QuestionActivity.EXTRA_QUESTIONID,
+									title.getId());
 							startActivity(oIntent);
 						}
-						});
+					});
 				}
-				
-				
+
 				// dynamically add tag buttons
-				PredicateLayout layout = (PredicateLayout) v.findViewById(R.id.tag_button_layout);
-				ArrayList<String> oTags = this.convertTagStringToList(o.getTagString());
-				for(String tag : oTags)
-				{
+				PredicateLayout layout = (PredicateLayout) v
+						.findViewById(R.id.tag_button_layout);
+				ArrayList<String> oTags = this.convertTagStringToList(o
+						.getTagString());
+				for (String tag : oTags) {
 					Button button = new Button(getContext());
 					button.setText(tag);
 					layout.addView(button);
 				}
 			}
-				
+
 			return v;
 		}
-		
+
 		/**
 		 * Converts a string of tags into an array list of tags
-		 * @param sTags string of tags in the following format <tag1><tags2>
+		 * 
+		 * @param sTags
+		 *            string of tags in the following format <tag1><tags2>
 		 * @return array list of tags
 		 */
-		private ArrayList<String> convertTagStringToList(String sTags){
+		private ArrayList<String> convertTagStringToList(String sTags) {
 			ArrayList<String> oTags = new ArrayList<String>();
-			for(String sTag : sTags.split(">"))
-			{
-				oTags.add(sTag.replace("<",""));
+			for (String sTag : sTags.split(">")) {
+				oTags.add(sTag.replace("<", ""));
 			}
 			return oTags;
 		}
 	}
-	
+
 }
