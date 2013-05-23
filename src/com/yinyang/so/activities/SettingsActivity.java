1f0e1ec90297aee44e0ea895c221c09088e29dfe
Mikey <mikey.rybacki@gmail.com>
2013-05-23 10:20:45 +0200

    Fixed slow fading, button order in the main_menu.xml, and back button in the settings activity

--- src/com/yinyang/so/activities/SettingsActivity.java
+++ src/com/yinyang/so/activities/SettingsActivity.java
@@ -4,7 +4,10 @@ import com.yinyang.so.R;
 import com.yinyang.so.fragments.SettingsFragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 /**
  * 
  * A screen showing only the settings fragment with available settings to toggle for the user
@@ -20,6 +23,19 @@ public class SettingsActivity extends Activity {
 	        getFragmentManager().beginTransaction()
 	                .replace(android.R.id.content, new SettingsFragment())
 	                .commit();
+			getActionBar().setHomeButtonEnabled(true);
+			getActionBar().setDisplayHomeAsUpEnabled(true);
 	    } 
+	  
+	    @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	            finish();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 	  
 	}
